63bf149a9befa45ebdbf2a837ad63abf8ab302f3
bobbihb <rholdingbrown@gmail.com>
2020-10-20 21:41:39 +0100

    improve error handling on form submit

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,26 +1,37 @@
 import {postData} from "./helpers";
+import {isURL} from "./urlValidator";
 
 async function handleSubmit(event) {
     event.preventDefault();
 
     // check what text was put into the form field
-    let feelingsText = document.getElementById('diary-entry').value;
+    let urlText = document.getElementById('diary-entry').value;
 
-    await postData('http://localhost:8000/feelings', feelingsText)
-        .then((res) => {
-            console.log(Object.keys(res).length);
-            if (Object.keys(res).length > 1) {
-                console.log('success!');
-                console.log(res["irony"]);
-                document.getElementById('results').innerHTML = `Your sentence: ${feelingsText}`;
-                document.getElementById('irony').innerHTML = `Irony: ${res["irony"]}`;
-                document.getElementById('subjectivity').innerHTML = `Subjectivity: ${res["subjectivity"]}`;
-            } else if (Object.keys(res).length === 1) {
-                console.log(res["errorData"]);
-                document.getElementById('results').innerHTML = res["errorData"];
-            }
+    if (isURL(urlText)) {
+        await postData('http://localhost:8000/feelings', urlText)
+            .then((res) => {
+                if ('code' in res.status) {
+                    console.log('error', res.code)
+                    if (res.status.code == 0) {
+                        console.log('success!');
+                        document.getElementById('results').innerHTML = `Your URL: ${res["sentence_list"][0]["text"]}`;
+                        document.getElementById('irony').innerHTML = `Irony: ${res["irony"]}`;
+                        document.getElementById('subjectivity').innerHTML = `Subjectivity: ${res["subjectivity"]}`;
+                    } else if (res.status.code == 100) {
+                        console.log('100 error')
+                        document.getElementById('results').innerHTML = "Make sure your credentials are working properly!";
+                    }
+                } else {
+                    console.log('noooo :(', res.status.code);
+                    document.getElementById('results').innerHTML = res["errorData"];
+                }
 
-        })
+            })
+    } else {
+        document.getElementById('results').innerHTML = "Invalid URL! Please check the URL provided and try again."
+    }
+
+
 
 }
 
